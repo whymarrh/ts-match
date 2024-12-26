72d584ce4eb490f0711d150d318d7e258c9769bd
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2024-12-26 14:17:59 -0330

    Simplify project dependencies; reformat
    A few structural changes here:
    
    - Use Biome
    - Use the built-in test runner, available in the LTS
    - Upgrade TypeScript version

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 type Predicate<T> = (t: T) => boolean;
 type Function<T, R> = (t: T) => R;
-type Type<T> = new(...args: any[]) => T;
+type Type<T> = new (...args: any[]) => T;
 
 interface ICase<T, R> {
   matches: Predicate<T>;
@@ -14,22 +14,21 @@ interface IDefaultCase<R> {
 function Case<R>(value: string, fn: Function<string, R>): ICase<string, R>;
 function Case<R>(value: number, fn: Function<number, R>): ICase<number, R>;
 function Case<T, R>(value: Type<T>, fn: Function<T, R>): ICase<T, R>;
-function Case<R> (value: any, fn: Function<any, R>): ICase<any, R> {
+function Case<R>(value: any, fn: Function<any, R>): ICase<any, R> {
   return {
     fn,
-    matches: (x) => (
+    matches: (x) =>
       (typeof value === 'function' && x instanceof value) ||
-            (typeof value === typeof x && value === x)
-    ),
+      (typeof value === typeof x && value === x),
   };
 }
 
 function DefaultCase<R>(): IDefaultCase<void>;
 function DefaultCase<R>(val: R | (() => R)): IDefaultCase<R>;
-function DefaultCase<R> (val?: any): IDefaultCase<R | void> {
+function DefaultCase<R>(val?: any): IDefaultCase<R | void> {
   if (!val) {
     return {
-      fn: (() => undefined),
+      fn: () => undefined,
     };
   }
   if (typeof val === 'function') {
@@ -39,11 +38,15 @@ function DefaultCase<R> (val?: any): IDefaultCase<R | void> {
   }
 
   return {
-    fn: (() => val),
+    fn: () => val,
   };
 }
 
-function match<T, R> (input: T, defaultCase: IDefaultCase<R>, ...cases: ICase<T, R>[]): R {
+function match<T, R>(
+  input: T,
+  defaultCase: IDefaultCase<R>,
+  ...cases: ICase<T, R>[]
+): R {
   for (const k of cases) {
     if (k.matches(input)) {
       return k.fn(input);
@@ -52,4 +55,13 @@ function match<T, R> (input: T, defaultCase: IDefaultCase<R>, ...cases: ICase<T,
   return defaultCase.fn();
 }
 
-export { Predicate, Function, Type, ICase, Case, IDefaultCase, DefaultCase, match };
+export {
+  type Predicate,
+  type Function,
+  type Type,
+  type ICase,
+  Case,
+  type IDefaultCase,
+  DefaultCase,
+  match,
+};
--- test/index.ts
+++ test/index.ts
@@ -1,109 +1,120 @@
-import * as test from 'tape';
-import { Case as when, DefaultCase as _, match } from '../lib';
+import * as assert from 'node:assert/strict';
+import { test } from 'node:test';
+import { DefaultCase as _, match, Case as when } from '../lib';
 
-test(`Matching numbers with a single failing match returns the default case value`, (t) => {
-  t.plan(1);
-
-  const result = match(1,
+test('Matching numbers with a single failing match returns the default case value', () => {
+  const result = match(
+    1,
     _(-1),
-    when(4, (x) => x + 1),);
-  t.equal(-1, result);
+    when(4, (x) => x + 1),
+  );
+
+  assert.equal(-1, result);
 });
 
-test(`Matching numbers with a single failing match uses the result of a default value fn`, (t) => {
-  t.plan(1);
-
-  const result = match<number, number>(1,
+test('Matching numbers with a single failing match uses the result of a default value fn', () => {
+  const result = match<number, number>(
+    1,
     _(() => 42),
-    when(-1, (x) => x + 1),);
-  t.equal(42, result);
+    when(-1, (x) => x + 1),
+  );
+  assert.equal(42, result);
 });
 
-test(`Matching numbers with a single passing match returns the result of the fn`, (t) => {
-  t.plan(1);
-
-  const result = match(1,
+test('Matching numbers with a single passing match returns the result of the fn', () => {
+  const result = match(
+    1,
     _(-1),
-    when(1, (x) => x + 1),);
-  t.equal(2, result);
+    when(1, (x) => x + 1),
+  );
+  assert.equal(2, result);
 });
 
-test(`Matching strings with a single passing match returns the result of the fn`, (t) => {
-  t.plan(1);
-
-  const result = match(`Lorem ipsum`,
+test('Matching strings with a single passing match returns the result of the fn', () => {
+  const result = match(
+    'Lorem ipsum',
     _(-1),
-    when(`Lorem ipsum`, (x) => x.length),);
-  t.equal(11, result);
+    when('Lorem ipsum', (x) => x.length),
+  );
+  assert.equal(11, result);
 });
 
-test(`Matching strings with a single failing match returns the default case value`, (t) => {
-  t.plan(1);
-
-  const result = match(`Lorem ipsum`,
+test('Matching strings with a single failing match returns the default case value', () => {
+  const result = match(
+    'Lorem ipsum',
     _(-42),
-    when(`Wat`, (s) => s.length),);
-  t.equal(-42, result);
+    when('Wat', (s) => s.length),
+  );
+  assert.equal(-42, result);
 });
 
-test(`Matching strings with a single failing match uses the result of a default value fn`, (t) => {
-  t.plan(1);
-
-  const result = match(`Lorem ipsum`,
+test('Matching strings with a single failing match uses the result of a default value fn', () => {
+  const result = match(
+    'Lorem ipsum',
     _(() => -42),
-    when(`Wat`, (s) => s.length),);
-  t.equal(-42, result);
+    when('Wat', (s) => s.length),
+  );
+  assert.equal(-42, result);
 });
 
 class A {
-  constructor (public readonly a: number, public readonly name: string) { /* empty */ }
+  constructor(
+    public readonly a: number,
+    public readonly name: string,
+  ) {
+    /* empty */
+  }
 }
 class B {
-  constructor (public readonly b: number, public readonly name: string) { /* empty */ }
+  constructor(
+    public readonly b: number,
+    public readonly name: string,
+  ) {
+    /* empty */
+  }
 }
 class C {
-  constructor (public readonly c: number, public readonly name: string) { /* empty */ }
+  constructor(
+    public readonly c: number,
+    public readonly name: string,
+  ) {
+    /* empty */
+  }
 }
 type Foo = A | B | C;
 
-test(`Matching types with a single failing match returns the default case value`, (t) => {
-  t.plan(1);
-
-  const s: Foo = new B(10, `busy bee`);
-  const result = match<Foo, number>(s,
+test('Matching types with a single failing match returns the default case value', () => {
+  const s: Foo = new B(10, 'busy bee');
+  const result = match<Foo, number>(
+    s,
     _(-42),
-    when(A, (s) => s.a),);
-  t.equal(-42, result);
+    when(A, (s) => s.a),
+  );
+  assert.equal(-42, result);
 });
 
-test(`Matching types with a single failing match uses the result of a default value fn`, (t) => {
-  t.plan(1);
-
-  const s: Foo = new B(10, `busy bee`);
+test('Matching types with a single failing match uses the result of a default value fn', () => {
+  const s: Foo = new B(10, 'busy bee');
   const result = match<Foo, number>(
     s,
     _(() => -42),
     when(A, (s) => s.a),
   );
-  t.equal(-42, result);
+  assert.equal(-42, result);
 });
 
-test(`Matching types with a single match returns the result of the match`, (t) => {
-  t.plan(1);
-
-  const s: Foo = new A(55, `test string`);
+test('Matching types with a single match returns the result of the match', () => {
+  const s: Foo = new A(55, 'test string');
   const result = match<Foo, number>(
     s,
     _(-42),
     when(A, (s) => s.a),
   );
-  t.equal(55, result);
+  assert.equal(55, result);
 });
 
-test(`Matching types with multiple match cases matches the value correctly and returns the result of the match`, (t) => {
-  t.plan(1);
-
-  const s: Foo = new C(5, `test string`);
+test('Matching types with multiple match cases matches the value correctly and returns the result of the match', () => {
+  const s: Foo = new C(5, 'test string');
   const result = match<Foo, number>(
     s,
     _(-42),
@@ -111,56 +122,48 @@ test(`Matching types with multiple match cases matches the value correctly and r
     when(B, (s) => s.b),
     when(C, (s) => s.c),
   );
-  t.equal(5, result);
+  assert.equal(5, result);
 });
 
-test(`Matching types to types with multiple match cases matches correctly and returns the result of the match`, (t) => {
-  t.plan(1);
-
-  const s: Foo = new C(5, `test string`);
+test('Matching types to types with multiple match cases matches correctly and returns the result of the match', () => {
+  const s: Foo = new C(5, 'test string');
   const result = match<Foo, Foo>(
     s,
-    _(new B(3, `foo`)),
+    _(new B(3, 'foo')),
     when(A, (s) => new A(s.a, s.name)),
     when(B, (s) => new B(s.b, s.name)),
-    when(C, (s) => new C(s.c, `Success`)),
+    when(C, (s) => new C(s.c, 'Success')),
   );
-  t.deepEqual(result.name, `Success`);
+  assert.deepEqual(result.name, 'Success');
 });
 
-test(`Matching types with multiple match cases matches the value correctly and returns nothing`, (t) => {
-  t.plan(1);
-
-  const s: Foo = new B(42, `test string`);
+test('Matching types with multiple match cases matches the value correctly and returns nothing', () => {
+  const s: Foo = new B(42, 'test string');
   match<Foo, void>(
     s,
     _(),
-    when(B, (_) => t.pass(`Pass`)),
-    when(A, (_) => undefined),
-    when(C, (_) => undefined),
+    when(B, (_) => undefined),
+    when(A, (_) => assert.fail('Should not match')),
+    when(C, (_) => assert.fail('Should not match')),
   );
 });
 
-test(`Matching values evaluates the default function if no cases match`, (t) => {
-  t.plan(1);
-
+test('Matching values evaluates the default function if no cases match', () => {
   match<number, void>(
     4,
-    _(() => t.pass(`Pass`)),
-    when(1, (n) => t.fail(`The case for ${n} should not match`)),
-    when(2, (n) => t.fail(`The case for ${n} should not match`)),
-    when(3, (n) => t.fail(`The case for ${n} should not match`)),
+    _(() => assert.ok(true)),
+    when(1, (n) => assert.fail(`The case for ${n} should not match`)),
+    when(2, (n) => assert.fail(`The case for ${n} should not match`)),
+    when(3, (n) => assert.fail(`The case for ${n} should not match`)),
   );
 });
 
-test(`Matching values does NOT evaluate the default function when a case matches`, (t) => {
-  t.plan(1);
-
+test('Matching values does NOT evaluate the default function when a case matches', () => {
   match<number, void>(
     1,
-    _(() => t.fail(`The default case should not be evaluated`)),
-    when(1, (_) => t.pass()),
-    when(2, (n) => t.fail(`The case for ${n} should not match`)),
-    when(3, (n) => t.fail(`The case for ${n} should not match`)),
+    _(() => assert.fail('The default case should not be evaluated')),
+    when(1, (_) => assert.ok(true)),
+    when(2, (n) => assert.fail(`The case for ${n} should not match`)),
+    when(3, (n) => assert.fail(`The case for ${n} should not match`)),
   );
 });
